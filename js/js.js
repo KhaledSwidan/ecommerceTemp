8cb6cfd7b1a5a54e07ad5f4f6677e3c26387fbae
khaled swidan <khaledqutb4@gmail.com>
2022-06-19 14:49:00 +0200

    categorySection1

--- js/js.js
+++ js/js.js
@@ -6,20 +6,20 @@ for (let i = 0; i < switchNum.length; i++){
     console.log(swichList);
     console.log(imgs);
     console.log("#".repeat(10));
-}
-swichList.forEach((li) => {
-    li.addEventListener("click", function () {
-        swichList.forEach((li) => {
-            li.classList.remove("active");
-            this.classList.add("active");
-            if (this.classList.contains("active")) {
-                this.classList.add("bg-white");
-                li.classList.remove("bg-white");
-            }
+    swichList.forEach((li) => {
+        li.addEventListener("click", function () {
+            swichList.forEach((li) => {
+                li.classList.remove("active");
+                this.classList.add("active");
+                if (this.classList.contains("active")) {
+                    this.classList.add("bg-white");
+                    li.classList.remove("bg-white");
+                }
+            });
+        });
+        li.addEventListener("click", function () {
+            imgs.forEach((img) => { img.style.display = "none" });
+            document.querySelectorAll(this.dataset.sel).forEach((e) => { e.style.display = "block" });
         });
     });
-    li.addEventListener("click", function () {
-        imgs.forEach((img) => { img.style.display = "none" });
-        document.querySelectorAll(this.dataset.sel).forEach((e) => { e.style.display = "block" });
-    });
-});
+}
