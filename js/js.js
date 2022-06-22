73a0d3e474ad0501c89cab520c4e7097821264cd
khaled swidan <khaledqutb4@gmail.com>
2022-06-22 16:07:24 +0200

    beandSection-someChanges

--- js/js.js
+++ js/js.js
@@ -4,9 +4,12 @@ for (let i = 0; i < switchNum.length; i++){
     let swichList = document.querySelectorAll(`.switch-${switchNum[i]} li`);
     let imgs = document.querySelectorAll(`.gall-${switchNum[i]} img`);
 
-    swichList.forEach((li) => {
-        li.addEventListener("click", function () {
-            swichList.forEach((li) => {
+    swichList.forEach((li) =>
+    {
+        li.addEventListener("click", function ()
+        {
+            swichList.forEach((li) =>
+            {
                 li.classList.remove("active");
                 this.classList.add("active");
                 if (this.classList.contains("active")) {
@@ -15,9 +18,13 @@ for (let i = 0; i < switchNum.length; i++){
                 }
             });
         });
-        li.addEventListener("click", function () {
+        li.addEventListener("click", function ()
+        {
             imgs.forEach((img) => { img.style.display = "none" });
             document.querySelectorAll(this.dataset.sel).forEach((e) => { e.style.display = "block" });
         });
     });
 }
+
+
+
